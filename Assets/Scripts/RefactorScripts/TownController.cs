b19409ed9a1e5db1a8388024a052fe0b78234cf8
John <John.charles.dunning@gmail.com>
2019-09-04 11:49:41 +1000

    Update to enemy village town logic

--- Assets/Scripts/RefactorScripts/TownController.cs
+++ Assets/Scripts/RefactorScripts/TownController.cs
@@ -6,25 +6,37 @@ public class TownController : MonoBehaviour
 {
     public List<VillagerController> VillagersInTown = new List<VillagerController>();
     public List<Building> BuildingsInTown = new List<Building>();
+
     public Transform BuildTester;
+
     public GameObject _Villager;
+    public GameObject[] _Buildings;
+    private ResourceType _RequiredResource;
     public Transform _Spawn;
 
     public int wood;
     public int stone;
     public int food;
 
-    public GameObject[] _Buildings;
+    public int _CurrentVillagerCount = 0;
+    public int _MaxVillagers = 5;
+
     public float MinDist, MaxDist;
 
     public Vector3 PlaceToBuild;
+
     private bool tryingToFindPlaceToBuild = false;
+
+    public MarketCost _Costs;
+
+
     void SpawnVillager()
     {
+        
         GameObject Villager = Instantiate(_Villager, _Spawn.position, Quaternion.identity);
         Villager.GetComponent<VillagerController>()._Task = VillagerTask.GatherResources;
         Villager.GetComponent<VillagerController>()._WantedGoal = ResourceType.wood;
-
+        VillagersInTown.Add(Villager.GetComponent<VillagerController>());
         int randRoll = 0;
         randRoll = Random.Range(0, 5);
         switch (randRoll)
@@ -46,7 +58,21 @@ public class TownController : MonoBehaviour
                 Villager.GetComponent<VillagerController>()._WantedGoal = ResourceType.combat;
                 break;
         }
-
+        if (wood < 50)
+        {
+            Villager.GetComponent<VillagerController>()._Task = VillagerTask.GatherResources;
+            Villager.GetComponent<VillagerController>()._WantedGoal = ResourceType.wood;
+        }
+        if (stone < 50)
+        {
+            Villager.GetComponent<VillagerController>()._Task = VillagerTask.GatherResources;
+            Villager.GetComponent<VillagerController>()._WantedGoal = ResourceType.stone;
+        }
+        if (food < 50)
+        {
+            Villager.GetComponent<VillagerController>()._Task = VillagerTask.GatherResources;
+            Villager.GetComponent<VillagerController>()._WantedGoal = ResourceType.food;
+        }
         Villager.GetComponent<VillagerController>()._Nav.SetDestination(_Spawn.position);
     }
 
@@ -58,37 +84,82 @@ public class TownController : MonoBehaviour
         StartCoroutine(PlaceBuilding());
     }
 
+    private void Update()
+    {
+        
+    }
+    bool CheckIfCanAfford(Vector3 ItemToBuy) //Accesses global costs and finds out if it can afford the specific item.
+    {
+        if(wood >= ItemToBuy.x && stone >= ItemToBuy.y && food >= ItemToBuy.z)
+        {
+            //Deducts the costs
+            wood -= (int)ItemToBuy.x;
+            stone -= (int)ItemToBuy.y;
+            food -= (int)ItemToBuy.z;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     IEnumerator SpawnNewVillager()
     {
         yield return new WaitForSeconds(10);
-        SpawnVillager();
+        if (CheckIfCanAfford(_Costs._Villager) == true)
+        {
+            SpawnVillager();
+            _CurrentVillagerCount++;
+
+        }       
+
         StartCoroutine(SpawnNewVillager());
     }
     IEnumerator PlaceBuilding()
     {
+
         GameObject SelectedBuilding = _Buildings[Random.Range(0, _Buildings.Length)];
         GameObject SelectedCurrentBuilding = BuildingsInTown[Random.Range(0, BuildingsInTown.Count)].gameObject;
+
         float buildSize = SelectedBuilding.GetComponent<Building>()._BuildingDetails._ConstructionSize /2;
+
         BuildTester.localScale = new Vector3(buildSize, buildSize, buildSize);
+
         yield return new WaitForSeconds(1);
+
         float xpos, ypos, zpos;
+
         xpos = SelectedCurrentBuilding.transform.position.x;
         ypos = SelectedCurrentBuilding.transform.position.y;
         zpos = SelectedCurrentBuilding.transform.position.z;
 
         Vector3 BuildPos = new Vector3(Random.insideUnitCircle.x * MaxDist + xpos, ypos, Random.insideUnitCircle.y * MaxDist + zpos);
+
         if (Vector3.Distance(BuildPos, SelectedBuilding.transform.position)> MinDist)
         {
             BuildTester.transform.position = BuildPos;
+
             yield return new WaitForSeconds(0.5f);
+
             if (BuildTester.GetComponent<BuildTest>().collisionCount == 0)
             {
-                GameObject building = Instantiate(SelectedBuilding, BuildPos, Quaternion.Euler(new Vector3(0, Random.Range(0, 270), 0)));
-                TownBuildings.Add(building);
+                //Logic for how enemy to decide what to place.
+                if(food < 50)
+                {
+                    if (CheckIfCanAfford(_Costs._Farm))
+                    {
+                        GameObject building = Instantiate(_Buildings[2], BuildPos, Quaternion.Euler(new Vector3(0, Random.Range(0, 270), 0)));
+                        TownBuildings.Add(building);
+                    }                        
+                }
+                if(_CurrentVillagerCount >= (_MaxVillagers * 0.8f))
+                {
+                    GameObject building = Instantiate(_Buildings[3], BuildPos, Quaternion.Euler(new Vector3(0, Random.Range(0, 270), 0)));
+                    TownBuildings.Add(building);
+                    _MaxVillagers += 5;
+                }
             }
         }
-        
-        
         StartCoroutine(PlaceBuilding());
     }
     
---
+++ Assets/Scripts/Resources/MarketCost.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "MarketCost", menuName = "Market", order = 1)]
+public class MarketCost : ScriptableObject
+{
+    [Header("Cost of goods")]
+    [Header("X=Wood,Y=Stone,Z=Food")]
+    [Space]
+    public Vector3 _Villager;
+    public Vector3 _House;
+    public Vector3 _Campfire;
+    public Vector3 _ResourceCache;
+    public Vector3 _Farm;
+    public Vector3 _Windmill;
+    public Vector3 _Barracks;
+    public Vector3 _TownCenter;
+    
+
+}
